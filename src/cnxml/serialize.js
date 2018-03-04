dbdde3488aaf2f3d8a6573928ea6658543e93830
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-03-04 17:39:59 +0100

    Add CNXML serializers

---
+++ src/cnxml/serialize.js
@@ -0,0 +1,115 @@
+import React from 'react'
+
+
+/**
+ * Serialize a block element.
+ *
+ * @see BLOCK_TAGS
+ */
+const BLOCK = {
+    serialize(obj, children) {
+        const Block = BLOCK_TAGS[obj.type]
+        if (!Block) return
+
+        if (Block instanceof Function) return Block(obj, children)
+
+        return <Block id={obj.key} {...obj.data.toJS()}>
+            {children}
+        </Block>
+    },
+}
+
+
+/**
+ * Serialize a mark.
+ *
+ * @see MARK_TAGS
+ */
+const MARK = {
+    serialize(obj, children) {
+        const Mark = MARK_TAGS[obj.type]
+        if (!Mark) return
+
+        if (Mark instanceof Function) return Mark(obj, children)
+
+        return <Mark {...obj.data.toJS()}>
+            {children}
+        </Mark>
+    },
+}
+
+
+/**
+ * Tags which can occur in block content.
+ *
+ * Keys are Slate node types, values are _transformer functions_. Transformer
+ * functions map Slate nodes to CNXML elements.
+ *
+ * As a special case, if value is a string then the node is serialized as a tag
+ * with name determined by the value, its `id` property set to Slate key, its
+ * data attributes turned into CNXML attributes, and its children serialized
+ * by Slate with no changes.
+ *
+ * @see BLOCK
+ */
+const BLOCK_TAGS = {
+    admonition: 'note',
+    exercise: 'exercise',
+    exercise_commentary: 'commentary',
+    exercise_problem: 'problem',
+    exercise_solution: 'solution',
+    figure: figure,
+    figure_caption: 'caption',
+    image: 'image',
+    media: 'media',
+    paragraph: 'para',
+    section: 'section',
+    title: 'title',
+}
+
+
+/**
+ * Serializer for CNXML emphasis tag.
+ */
+const emphasis = type => function(obj, children) {
+    return <emphasis effect={type}>{children}</emphasis>
+}
+
+
+/**
+ * Mark serialisation works similarly to block serialisation, except the default
+ * function (the one used when value is a string) doesn't produce the
+ * `id` attribute.
+ *
+ * @see MARK
+ * @see BLOCK_TAGS
+ */
+const MARK_TAGS = {
+    emphasis: emphasis('italics'),
+    strong: emphasis('bold'),
+    underline: emphasis('underline'),
+    subscript: 'sub',
+    superscript: 'sup',
+}
+
+
+/**
+ * Serializer for figures.
+ */
+function figure(obj, children) {
+    console.log('figure', children)
+
+    return <figure id={obj.key}>
+        {/* We need to turn nested figures from <figure>s to <subfigure>s */}
+        {children.map(el => el.type !== 'figure' ? el : {
+            ...el,
+            type: 'subfigure',
+        })}
+    </figure>
+}
+
+
+export default [
+    BLOCK,
+    MARK,
+]
