28b8db4561e0c7f48ed77b6c992c2d51439c56b2
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-03-04 19:08:07 +0100

    Define server storage API

---
+++ src/storage/storage.js
@@ -0,0 +1,46 @@
+import CNXML from '../cnxml'
+
+
+export default class Storage {
+    /**
+     * Load a document by ID.
+     *
+     * Retrieves basic information about a document and prepares
+     * a {@link Storage} for later work.
+     *
+     * @return {Storage}
+     */
+    static async load(id) {
+        // TODO: implement
+    }
+
+    /**
+     * Read the document.
+     *
+     * @return {Value}
+     */
+    async read() {
+        // TODO: implement
+    }
+
+    /**
+     * Write the document
+     */
+    async write(value) {
+        // TODO: implement
+    }
+
+    /**
+     * Check if a {@link Value} is current.
+     */
+    current(value) {
+        // TODO: implement
+    }
+
+    /**
+     * Return an URL for a given media file.
+     */
+    mediaUrl(name) {
+        // TODO: implement
+    }
+}
