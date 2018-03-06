e07e467e4597866ff043220bb8e30455cd14b2f0
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-03-06 19:13:57 +0100

    Create new modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import { Value } from 'slate'
 
 import Editor from './editor'
@@ -114,6 +114,142 @@ class Modules extends React.Component {
 }
 
 
+class New extends React.Component {
+    state = {
+        license: undefined,
+        stage: 'license',
+        title: "",
+        files: [],
+    }
+
+    componentDidMount() {
+        this.loadLicenses()
+    }
+
+    render() {
+        const { stage } = this.state
+
+        return <div className="new">
+            {stage === 'input' ? this.renderForm() : null}
+            {stage === 'create' ? <span>Creating module</span> : null}
+            {stage === 'upload' ? <span>Uploading files</span> : null}
+        </div>
+    }
+
+    renderForm() {
+        return <form onSubmit={this.submit}>
+            <input
+                type="text"
+                value={this.state.value}
+                placeholder="Title"
+                onChange={ev =>
+                    this.setState({ title: ev.target.value })
+                } />
+
+            <label>
+                <span>License</span>
+                <select
+                    value={this.state.license}
+                    onChange={ev => this.setState({ license: Number(ev.target.value) })}
+                    >
+                    {this.state.licenses.map(license =>
+                        <option key={license.id} value={license.id}>
+                            {license.name}
+                        </option>
+                    )}
+                </select>
+            </label>
+
+            <div>
+                <input
+                    type="file" multiple
+                    ref={input => {this.fileInput = input}}
+                    style={{display: 'none'}}
+                    onChange={this.onFiles}
+                    />
+                <button onClick={this.addFiles}>Add files</button>
+                {this.state.files.map(file => this.renderFile(file))}
+            </div>
+
+            <input type="submit" value="Create" />
+        </form>
+    }
+
+    renderFile({ file, type }) {
+        return <div className="file" key={file.name}>
+            <span>{file.name}</span>
+            <span>{type}</span>
+        </div>
+    }
+
+    submit = ev => {
+        ev.preventDefault()
+        this.setState({ stage: 'create' })
+        this.create()
+    }
+
+    addFiles = ev => {
+        ev.preventDefault()
+        this.fileInput.click()
+    }
+
+    onFiles = ev => {
+        const files = []
+
+        for (const file of ev.target.files) {
+            let type = file.type
+
+            if (type === '' && file.name === 'index.cnxml') {
+                type = 'application/vnd.openstax.cnx+xml'
+            }
+
+            files.push({ file, type })
+        }
+
+        this.setState({ files })
+    }
+
+    async loadLicenses() {
+        const req = await fetch('/licenses')
+        const licenses = await req.json()
+        this.setState({
+            stage: 'input',
+            licenses: licenses,
+            license: licenses[0].id,
+        })
+    }
+
+    async create() {
+        const data = new URLSearchParams()
+        data.set('kind', 'module')
+        data.set('name', this.state.title)
+        data.set('license', this.state.license)
+
+        const req = await fetch('/user/contents', {
+            credentials: 'same-origin',
+            method: 'post',
+            body: data,
+        })
+        const module = await req.json()
+
+        await Promise.all(this.state.files.map(async ({ file, type }) => {
+            const path = '/user/contents/' + module.id + '/files/' + file.name
+            const headers = new Headers()
+            headers.set('Content-Type', type)
+
+            const req = await fetch(path, {
+                credentials: 'same-origin',
+                method: 'put',
+                headers: headers,
+                body: file,
+            })
+        }))
+
+        this.props.history.replace('/' + module.id)
+    }
+}
+
+
 class Root extends React.Component {
     state = {
         user: null,
@@ -121,10 +257,11 @@ class Root extends React.Component {
 
     render() {
         return <Router>
-            <React.Fragment>
+            <Switch>
                 <Route exact path="/" component={Modules} />
+                <Route exact path="/new" component={New} />
                 <Route exact path="/:id" component={App} />
-            </React.Fragment>
+            </Switch>
         </Router>
     }
 }
