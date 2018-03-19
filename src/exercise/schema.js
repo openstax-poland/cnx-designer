17392fe296807cb1781b579f49a71345e4bd8437
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-03-19 12:30:58 +0100

    Improve exercise normalisation
    Selection will now be kept as it was.

--- src/exercise/schema.js
+++ src/exercise/schema.js
@@ -34,11 +34,7 @@ function normalizeExercise(change, violation, context) {
         }
         // Otherwise (invalid child after the problem but before the commentary)
         // we lift the invalid child into a new solution.
-        const solution = Block.create({
-            type: 'exercise_solution',
-            nodes: [child],
-        })
-        change.replaceNodeByKey(child.key, solution)
+        change.wrapBlockByKey(child.key, 'exercise_solution')
         break
 
     // No child was expected.
