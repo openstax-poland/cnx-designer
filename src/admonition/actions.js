59b6a355cc1cdf43a9a5194284766f3d6eea03ae
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-03-19 16:43:37 +0100

    Implement changing admonition type

--- src/admonition/actions.js
+++ src/admonition/actions.js
@@ -2,17 +2,30 @@
 
 import createMenu from '../actions'
 
+import * as utils from './utils'
+
+
+function insertAdmonition(change, type) {
+    const admonition = utils.getCurrentAdmonition(change.value)
+
+    if (admonition) {
+        change.setNodeByKey(admonition.key, { data: { type } })
+    } else {
+        change.wrapBlock({
+            type: 'admonition',
+            data: {
+                type: type,
+            },
+        })
+    }
+}
+
 
 function Action({ type, ...attrs }) {
     const title = type.replace(/^\w/, x => x.toUpperCase())
     return <action
         title={title}
-        action={change => change.wrapBlock({
-            type: 'admonition',
-            data: {
-                type: type,
-            },
-        })}
+        action={change => insertAdmonition(change, type)}
         {...attrs}
         />
 }
