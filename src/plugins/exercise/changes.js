21d5de7cfe4d8a8e5eae9a4f72aba5740dcfe955
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-10-04 14:29:35 +0200

    Upgrade to slate 0.41.0, slate-react 0.18.0

--- src/plugins/exercise/changes.js
+++ src/plugins/exercise/changes.js
@@ -9,111 +9,106 @@ import { EXERCISE_PARENT } from './schema'
  * @param {Slate~Change} change
  */
 export function insertExercise(change) {
-    const { value } = change
-    const { document } = value
+    change.withoutNormalizing(change => {
+        const { value } = change
+        const { document } = value
 
-    let first = value.startBlock
-    let last = value.endBlock
+        let first = value.startBlock
+        let last = value.endBlock
 
-    // Find the lowest common ancestor of nodes in selected range.
-    let parent = document.getClosest(first.key, p1 => {
-        return !!document.getClosest(last.key, p2 => p1 === p2)
-    }) || document
+        // Find the lowest common ancestor of nodes in selected range.
+        let parent = document.getClosest(first.key, p1 => {
+            return !!document.getClosest(last.key, p2 => p1 === p2)
+        }) || document
 
-    // When using `change.wrapBlock` slate will place blocks according to its
-    // own rules, which don't take into account legal parent-child relations,
-    // which may cause exercise to be created as a child of e.g. a list,
-    // and then promptly removed by normalizations. To avoid this we only use
-    // `change.wrapBlock` when we're sure output will be OK.
-    if (EXERCISE_PARENT.includes(parent.type)) {
-        return change.withoutNormalization(change => {
+        // When using `change.wrapBlock` slate will place blocks according to its
+        // own rules, which don't take into account legal parent-child relations,
+        // which may cause exercise to be created as a child of e.g. a list,
+        // and then promptly removed by normalizations. To avoid this we only use
+        // `change.wrapBlock` when we're sure output will be OK.
+        if (EXERCISE_PARENT.includes(parent.type)) {
             change.wrapBlock('exercise')
             change.wrapBlock('exercise_problem')
+            return
+        }
+
+        // Adjust selection such that it begins on a direct descendant of |parent|.
+        for (;;) {
+            const pp = change.value.document.getParent(first.key)
+            if (pp.key === parent.key) break
+
+            // If |first| is the first child of its parent (|pp|) then we can just
+            // move selection onto |pp|, otherwise we need to split |pp|, as we'll
+            // only be moving some of its contents into the new exercise.
+            const index = pp.nodes.indexOf(first)
+            if (index === 0) {
+                first = pp
+            } else {
+                change.splitNodeByKey(pp.key, index)
+                first = change.value.document.getParent(first.key)
+            }
+        }
+
+        // Adjust selection such that it ends on a direct descendant of |parent|.
+        for (;;) {
+            const pp = change.value.document.getParent(last.key)
+            if (pp.key === parent.key) break
+
+            // See rationale in the previous loop.
+            const index = pp.nodes.indexOf(last)
+            if (index + 1 === pp.nodes.size) {
+                last = pp
+            } else {
+                change.splitNodeByKey(pp.key, index + 1)
+                last = change.value.document.getParent(last.key)
+            }
+        }
+
+        // Previous two loops might have changed |parent|.
+        parent = change.value.document.getDescendant(parent.key)
+
+        // At this point the selection begins and ends on direct children
+        // of |parent|, which means we can start unwrapping nodes out if it.
+
+        while (!EXERCISE_PARENT.includes(parent.type)) {
+            if (first === last) {
+                // Simple case, Slate can handle it on its own.
+                change.unwrapNodeByKey(first.key)
+                parent = change.value.document.getParent(first.key)
+            } else {
+                const firstInx = parent.nodes.indexOf(first)
+                const lastInx = parent.nodes.indexOf(last) + 1
+
+                if (lastInx + 1 < parent.nodes.size) {
+                    change.splitNodeByKey(parent.key, lastInx)
+                }
+                if (firstInx > 0) {
+                    change.splitNodeByKey(parent.key, firstInx)
+                }
+
+                first = last = change.value.document.getParent(first.key)
+                parent = change.value.document.getParent(first.key)
+            }
+        }
+
+        // At this point the selected range is directly under a legal parent
+        // of exercise.
+
+        const start = parent.nodes.indexOf(first)
+        const end = parent.nodes.indexOf(last) + 1
+        const nodes = parent.nodes.slice(start, end)
+
+        // Note that nodes in |nodes| are still children of |parent|, so we can't
+        // pass them as to `Block.create`, we'll have to move them.
+        const problem = Block.create({ type: 'exercise_problem' })
+        const exercise = Block.create({ type: 'exercise', nodes: [problem] })
+        change.insertNodeByKey(parent.key, start, exercise)
+
+        // Now we can move |nodes| into |problem|.
+        nodes.forEach((node, inx) => {
+            change.moveNodeByKey(node.key, problem.key, inx)
         })
-    }
-
-    // Adjust selection such that it begins on a direct descendant of |parent|.
-    for (;;) {
-        const pp = change.value.document.getParent(first.key)
-        if (pp.key === parent.key) break
-
-        // If |first| is the first child of its parent (|pp|) then we can just
-        // move selection onto |pp|, otherwise we need to split |pp|, as we'll
-        // only be moving some of its contents into the new exercise.
-        const index = pp.nodes.indexOf(first)
-        if (index === 0) {
-            first = pp
-        } else {
-            change.splitNodeByKey(pp.key, index, { normalize: false })
-            first = change.value.document.getParent(first.key)
-        }
-    }
-
-    // Adjust selection such that it ends on a direct descendant of |parent|.
-    for (;;) {
-        const pp = change.value.document.getParent(last.key)
-        if (pp.key === parent.key) break
-
-        // See rationale in the previous loop.
-        const index = pp.nodes.indexOf(last)
-        if (index + 1 === pp.nodes.size) {
-            last = pp
-        } else {
-            change.splitNodeByKey(pp.key, index + 1, { normalize: false })
-            last = change.value.document.getParent(last.key)
-        }
-    }
-
-    // Previous two loops might have changed |parent|.
-    parent = change.value.document.getDescendant(parent.key)
-
-    // At this point the selection begins and ends on direct children
-    // of |parent|, which means we can start unwrapping nodes out if it.
-
-    while (!EXERCISE_PARENT.includes(parent.type)) {
-        if (first === last) {
-            // Simple case, Slate can handle it on its own.
-            change.unwrapNodeByKey(first.key, { normalize: false })
-            parent = change.value.document.getParent(first.key)
-        } else {
-            const firstInx = parent.nodes.indexOf(first)
-            const lastInx = parent.nodes.indexOf(last) + 1
-
-            if (lastInx + 1 < parent.nodes.size) {
-                change.splitNodeByKey(parent.key, lastInx, { normalize: false })
-            }
-            if (firstInx > 0) {
-                change.splitNodeByKey(parent.key, firstInx, { normalize: false })
-            }
-
-            first = last = change.value.document.getParent(first.key)
-            parent = change.value.document.getParent(first.key)
-        }
-    }
-
-    // At this point the selected range is directly under a legal parent
-    // of exercise.
-
-    const start = parent.nodes.indexOf(first)
-    const end = parent.nodes.indexOf(last) + 1
-    const nodes = parent.nodes.slice(start, end)
-
-    // Note that nodes in |nodes| are still children of |parent|, so we can't
-    // pass them as to `Block.create`, we'll have to move them.
-    const problem = Block.create({ type: 'exercise_problem' })
-    const exercise = Block.create({ type: 'exercise', nodes: [problem] })
-    change.insertNodeByKey(parent.key, start, exercise, { normalize: false })
-
-    // Now we can move |nodes| into |problem|.
-    nodes.forEach((node, inx) => {
-        change.moveNodeByKey(node.key, problem.key, inx, { normalize: false })
     })
-
-    // We've been doing all those changes with normalization turned off. Unless
-    // it was already disabled when we were called, run it now.
-    if (change.getFlag('normalize')) {
-        change.normalizeNodeByKey(parent.key)
-    }
 }
 
 /**
