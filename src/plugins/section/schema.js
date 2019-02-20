82460105eae24e8fb0720adb5db0254257d336f2
Krzysztof Mędrzycki <aiwenari@gmail.com>
2019-02-20 13:10:26 +0100

    Improve section and document schemas

--- src/plugins/section/schema.js
+++ src/plugins/section/schema.js
@@ -2,25 +2,106 @@
 // Licensed under the MIT license. See LICENSE file in the project root for
 // full license text.
 
-function normalizeSection(change, error) {
-    const { code } = error
+function normalizeDocument(change, error) {
+    const { code, node, child } = error
 
-    // TODO: insert default title when missing
     switch (code) {
+    // Document is empty.
+    case 'child_min_invalid':
+        change.insertNodeByKey(node.key, 0, {
+            object: 'block',
+            type: 'paragraph',
+        })
+        break
+
+    case 'child_type_invalid':
+        if (child.type === 'title') {
+            change.setNodeByKey(child.key, { type: 'paragraph' })
+            return
+        }
+        console.warn('Unhandled document violation:', error.code)
+        break
+
+    default:
+        console.warn('Unhandled document violation:', error.code)
+    }
+}
+
+function normalizeSection(change, error) {
+    const { code, node, child, next, index } = error
+
+    switch (code) {
+    case 'child_min_invalid':
+        if (index === 0) {
+            // Section has no title.
+            change.unwrapBlockByKey(node.key)
+            return
+        }
+        // Section has a title but doesn't have any content.
+        change.insertNodeByKey(node.key, 1, {
+            object: 'block',
+            type: 'paragraph',
+        })
+        break
+
+    case 'child_max_invalid':
+        // Two titles in a section. This usually happens when user presses enter
+        // in a title.
+        if (child.type === 'title') {
+            change.setNodeByKey(child.key, { type: 'paragraph' })
+            return
+        }
+        console.warn('Unhandled violation in section:', code)
+        break
+
+    // There is a block after section that is not a section itself.
+    case 'next_sibling_type_invalid':
+        // XXX: while we would expect |node| to refer to section it is in fact
+        // its parent.
+        let parent = node.nodes.get(index)
+        change.moveNodeByKey(next.key, parent.key, parent.nodes.size)
+        break
+
     default:
         console.warn('Unhandled violation in section:', code)
         break
     }
 }
 
+/**
+ * Nodes which may appear in a section and in the document.
+ */
+const DOCUMENT_NODES = [
+    { type: 'admonition' },
+    { type: 'exercise' },
+    { type: 'figure' },
+    { type: 'ol_list' },
+    { type: 'paragraph' },
+    { type: 'section' },
+    { type: 'ul_list' },
+]
+
 export default {
+    document: {
+        nodes: [
+            {
+                match: DOCUMENT_NODES,
+                min: 1,
+            },
+        ],
+        normalize: normalizeDocument,
+    },
     blocks: {
         section: {
-            // TODO:
-            // nodes: [
-            //     { type: 'title', min: 1, max: 1 },
-            // ]
+            nodes: [
+                { match: { type: 'title' }, min: 1, max: 1 },
+                {
+                    match: DOCUMENT_NODES,
+                    min: 1,
+                },
+            ],
+            next: { type: 'section' },
             normalize: normalizeSection,
         }
-    }
+    },
 }
