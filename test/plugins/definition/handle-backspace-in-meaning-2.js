afad65e696183e227335b3e6ec4e4dc0696d1567
Piotr Kozlowski <pior@obrandowani.pl>
2019-05-30 15:18:55 +0200

    Add more specific tests

---
+++ test/plugins/definition/handle-backspace-in-meaning-2.js
@@ -0,0 +1,28 @@
+/** @jsx h */
+
+export default (change, editor) => {
+    editor.run('onKeyDown', { key: 'Backspace' })
+}
+
+export const input = <value>
+    <document>
+        <definition>
+            <defterm>Term</defterm>
+            <defmeaning>
+                <p>Meaning</p>
+                <p><cursor/></p>
+            </defmeaning>
+        </definition>
+    </document>
+</value>
+
+export const output = <value>
+    <document>
+        <definition>
+            <defterm>Term</defterm>
+            <defmeaning>
+                <p>Meaning<cursor/></p>
+            </defmeaning>
+        </definition>
+    </document>
+</value>
