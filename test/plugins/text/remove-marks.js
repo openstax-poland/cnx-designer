b067925f5fd8e8587ff32c59eba8756ea69ab362
Krzysztof Mędrzycki <aiwenari@gmail.com>
2019-06-03 11:49:30 +0200

    Enable test for removeMarks()

--- test/plugins/text/remove-marks.js
+++ test/plugins/text/remove-marks.js
@@ -2,18 +2,14 @@
 
 export default editor => editor.removeMarks()
 
-// Currently after marks are removed we're left with a bunch of unmerged leafs,
-// which is hard to represent in hyperscript.
-export const skip = true
-
 export const input = <value>
     <document>
-        <p><b>Before</b>, <i>aft<u>er<cursor/>,</u></i> end</p>
+        <p><b>Before</b>, <i>aft<anchor/><u>er,</u></i><focus/> end</p>
     </document>
 </value>
 
 export const output = <value>
     <document>
-        <p><b>Before</b>, after<cursor/>, end</p>
+        <p><b>Before</b>, <i>aft</i><anchor/>er,<focus/> end</p>
     </document>
 </value>
