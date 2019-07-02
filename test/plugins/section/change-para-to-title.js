84baf91c37efda5935e760f7249a661b74295f5b
Piotr Kozlowski <pior@obrandowani.pl>
2019-07-02 15:23:25 +0200

    Add test for changing para to title inside section

---
+++ test/plugins/section/change-para-to-title.js
@@ -0,0 +1,29 @@
+/** @jsx h */
+
+export default editor => editor.setNodeByKey(editor.value.startBlock.key, { type: 'title' })
+
+export const input = <value>
+    <document>
+        <section>
+            <title>Section</title>
+            <p>Paragraph</p>
+            <p><cursor/>Para 1</p>
+            <p>Para 2</p>
+            <p>Para 3</p>
+        </section>
+    </document>
+</value>
+
+export const output = <value>
+    <document>
+        <section>
+            <title>Section</title>
+            <p>Paragraph</p>
+        </section>
+        <section>
+            <title><cursor/>Para 1</title>
+            <p>Para 2</p>
+            <p>Para 3</p>
+        </section>
+    </document>
+</value>
