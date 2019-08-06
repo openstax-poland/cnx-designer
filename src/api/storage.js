af079231b8fbca35cbb31a8a5c569a9d25e02586
Krzysztof Mędrzycki <aiwenari@gmail.com>
2019-08-06 10:50:06 +0200

    Remove unused API from Storage

--- src/api/storage.js
+++ src/api/storage.js
@@ -6,48 +6,6 @@
 
 export default class Storage {
     /**
-     * Load a document by ID.
-     *
-     * Retrieves basic information about a document and prepares
-     * a {@link Storage} for later work.
-     *
-     * @return {Storage}
-     */
-    static async load(id) {
-        throw new Error("not implemented")
-    }
-
-    /**
-     * Read the document.
-     *
-     * @return {Value}
-     */
-    async read() {
-        throw new Error("not implemented")
-    }
-
-    /**
-     * Write the document
-     */
-    async write(value) {
-        throw new Error("not implemented")
-    }
-
-    /**
-     * Write a file.
-     */
-    async writeFile(file) {
-        throw new Error("not implemented")
-    }
-
-    /**
-     * Check if a {@link Value} is current.
-     */
-    current(value) {
-        throw new Error("not implemented")
-    }
-
-    /**
      * Return an URL for a given media file.
      */
     mediaUrl(name) {
