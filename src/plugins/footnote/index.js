2483d3995c6e827a94eb0c3b73f237d102a44a11
Piotr Kozlowski <pior@obrandowani.pl>
2019-09-26 15:12:52 +0200

    Define default inlines which are allowed in footnote

--- src/plugins/footnote/index.js
+++ src/plugins/footnote/index.js
@@ -5,19 +5,21 @@
 import renderInline from './render'
 import make_schema from './schema'
 
+const ALLOWED_INLINES = ['code', 'docref', 'link', 'term', 'xref']
+
 /**
  * @param {string[]} options.data - Object with data items and their
  *                                  validation functions.
  * @param {string[]} options.marks - List of mark types which may appear inside
  *                                   a footnote.
  */
-export default function Term(options={}) {
+export default function Footnote(options={}) {
     const {
         marks = [],
         inlines = [],
     } = options
 
-    const schema = make_schema({ marks, inlines })
+    const schema = make_schema({ marks, inlines: ALLOWED_INLINES.concat(inlines) })
 
     return { renderInline, schema }
 }
