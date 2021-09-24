397a5644be19d31c40f58546b6f8f5850f1d62bb
Krzysztof Mędrzycki <aiwenari@gmail.com>
2021-09-24 10:27:28 +0200

    Publish cnx-designer 0.2.3

---
+++ dist/normalize/figure.js
@@ -0,0 +1,98 @@
+// Copyright 2020 OpenStax Poland
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license text.
+import { Editor, Transforms } from 'slate';
+import { Admonition, AltText, Audio, Caption, Figure, Image, Media, Section, Title, Video, } from '../interfaces';
+import { enumerate } from '../util';
+import { normalizeOrderedChildren } from './util';
+/**
+ * Normalize a figure
+ *
+ * Return true if entry was normalized and false otherwise.
+ */
+export default function normalizeFigure(editor, entry) {
+    const [node, path] = entry;
+    if (Figure.isFigure(node)) {
+        // Figure can only be a child of sections, admonitions, the document,
+        // and other figures.
+        const [parent, parentPath] = Editor.parent(editor, path);
+        if (!Section.isSection(parent) && !Figure.isFigure(parent)
+            && !Admonition.isAdmonition(parent) && parentPath.length > 0) {
+            Transforms.liftNodes(editor, { at: path });
+            return true;
+        }
+        // Nested figures can't contain subfigures.
+        if (Figure.isFigure(parent) && node.children.some(Figure.isFigure)) {
+            Transforms.unwrapNodes(editor, {
+                at: path,
+                match: n => n === node,
+            });
+            return true;
+        }
+        // Ensure children are ordered items then captions. At this point there
+        // may still be multiple captions interspersed with items.
+        if (normalizeOrderedChildren(editor, [node, path], [Title.isTitle, isFigureItem, Caption.isCaption], normalizeInvalidChild)) {
+            return true;
+        }
+        // At this point there can only be one caption, and it can only be the
+        // last child.
+        const hasCaption = Caption.isCaption(node.children[node.children.length - 1]);
+        const hasTitle = Title.isTitle(node.children[0]);
+        const length = node.children.length - (hasCaption ? 1 : 0) - (hasTitle ? 1 : 0);
+        // Figures must not be empty.
+        if (length === 0) {
+            Transforms.removeNodes(editor, { at: path });
+            return true;
+        }
+        // There must be more than one subfigure.
+        if (length === 1 && Figure.isFigure(node.children[0])) {
+            Transforms.unwrapNodes(editor, { at: [...path, 0] });
+            return true;
+        }
+        // If there is more than one item, all must be subfigures.
+        if (length > 1) {
+            for (const [inx, child] of enumerate(node.children)) {
+                if (Title.isTitle(child) || Figure.isFigure(child) || Caption.isCaption(child)) {
+                    continue;
+                }
+                Transforms.wrapNodes(editor, {
+                    type: 'figure',
+                    children: [],
+                }, { at: [...path, inx] });
+                return true;
+            }
+        }
+    }
+    if (Media.isMedia(node)) {
+        // Ensure children are ordered media items then alt text.
+        if (normalizeOrderedChildren(editor, [node, path], [isMediaItem, AltText.isAltText], normalizeInvalidChild)) {
+            return true;
+        }
+        const hasAltText = AltText.isAltText(node.children[node.children.length - 1]);
+        const length = node.children.length - (hasAltText ? 1 : 0);
+        // Media elements must contain at least one media item.
+        if (length < 1) {
+            Transforms.removeNodes(editor, { at: path });
+            return true;
+        }
+        // Media elements must contain alt text.
+        if (!hasAltText) {
+            Transforms.insertNodes(editor, {
+                type: 'media_alt',
+                children: [{ text: '' }],
+            }, { at: [...path, node.children.length] });
+            return true;
+        }
+    }
+    return false;
+}
+function isFigureItem(node) {
+    return Figure.isFigure(node) || Media.isMedia(node);
+}
+function isMediaItem(node) {
+    return Image.isImage(node) || Audio.isAudio(node) || Video.isVideo(node);
+}
+function normalizeInvalidChild(editor, entry) {
+    const [, path] = entry;
+    Transforms.unwrapNodes(editor, { at: path });
+}
---
+++ dist/transforms/glossary.js
@@ -0,0 +1,149 @@
+// Copyright 2020 OpenStax Poland
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license text.
+import { Editor, Path, Range, Transforms } from 'slate';
+import { Definition, Glossary, SeeAlso } from '../interfaces';
+/**
+ * Add a new definition to the glossary
+ *
+ * If there is no glossary in the document a new one will be created.
+ *
+ * If select is set to true the selection will be collapsed into the new
+ * definition.
+ */
+export function addGlossaryDefinition(editor, options = {}) {
+    Editor.withoutNormalizing(editor, () => {
+        const { at = editor.selection, mode = 'after', select } = options;
+        if (at == null)
+            return;
+        const node = {
+            type: 'definition',
+            children: [{
+                    type: 'definition_term',
+                    children: [{ text: '' }],
+                }],
+        };
+        // Fast path: there is no glossary in the document.
+        if (!Glossary.isGlossary(editor.children[editor.children.length - 1])) {
+            Transforms.insertNodes(editor, {
+                type: 'glossary',
+                children: [node],
+            }, { at: [editor.children.length], select });
+            return;
+        }
+        const [start, end] = Range.edges(Editor.range(editor, at));
+        const path = mode === 'before' ? start.path : end.path;
+        let [glossary, glossaryPath] = Editor.above(editor, {
+            at: path,
+            match: Glossary.isGlossary,
+        });
+        let index;
+        if (glossary == null) {
+            glossaryPath = [editor.children.length - 1];
+            glossary = Editor.node(editor, glossaryPath)[0];
+            index = mode === 'before' ? 0 : glossary.children.length;
+        }
+        else {
+            index = Path.relative(path, glossaryPath)[0]
+                + (mode === 'before' ? 0 : 1);
+        }
+        Transforms.insertNodes(editor, node, {
+            at: [...glossaryPath, index],
+            select,
+        });
+    });
+}
+/**
+ * Insert new item into a definition
+ *
+ * Does nothing if no definition is selected.
+ *
+ * If select is set to true the selection will be collapsed into the new item.
+ */
+function insertItem(editor, element, options = {}) {
+    Editor.withoutNormalizing(editor, () => {
+        const { at = editor.selection, select } = options;
+        if (at == null)
+            return;
+        const atPath = Editor.path(editor, at);
+        const [[definition, definitionPath]] = Editor.levels(editor, {
+            at: atPath,
+            match: Definition.isDefinition,
+        });
+        if (definition == null)
+            return;
+        const index = Path.relative(atPath, definitionPath)[0];
+        const item = definition.children[index];
+        const newPath = SeeAlso.isSeeAlso(item)
+            ? [...definitionPath, index]
+            : [...definitionPath, index + 1];
+        Transforms.insertNodes(editor, {
+            ...element,
+            children: [{
+                    type: 'paragraph',
+                    children: [{ text: "" }],
+                }],
+        }, { at: newPath, select });
+    });
+}
+/**
+ * Insert a new meaning into a definition
+ *
+ * Does nothing if no definition is selected.
+ */
+export function insertMeaning(editor, options = {}) {
+    insertItem(editor, { type: 'definition_meaning' }, options);
+}
+/**
+ * Insert a new example into a definition
+ *
+ * Does nothing if no definition is selected.
+ *
+ * If select is set to true the selection will be collapsed into the new
+ * example.
+ */
+export function insertDefinitionExample(editor, options = {}) {
+    insertItem(editor, { type: 'definition_example' }, options);
+}
+/**
+ * Insert a new see-also section into a definition.
+ *
+ * Does nothing if no definition is selected. If the definition already contains
+ * a see-also section, a new see-also term is added to it instead.
+ *
+ * If select is set to true the selection will be collapsed into the new
+ * see-also term.
+ */
+export function insertSeeAlso(editor, options = {}) {
+    Editor.withoutNormalizing(editor, () => {
+        const { at = editor.selection, select } = options;
+        if (at == null)
+            return;
+        const atPath = Editor.path(editor, at);
+        const [[definition, definitionPath]] = Editor.levels(editor, {
+            at: atPath,
+            match: Definition.isDefinition,
+        });
+        if (definition == null)
+            return;
+        const relPath = Path.relative(atPath, definitionPath);
+        const index = relPath[0];
+        const item = definition.children[index];
+        let newPath;
+        let node = {
+            type: 'definition_term',
+            children: [{ text: '' }],
+        };
+        if (SeeAlso.isSeeAlso(item)) {
+            newPath = [...definitionPath, index, relPath[1] + 1];
+        }
+        else {
+            newPath = [...definitionPath, definition.children.length];
+            node = {
+                type: 'definition_seealso',
+                children: [node],
+            };
+        }
+        Transforms.insertNodes(editor, node, { at: newPath, select });
+    });
+}
