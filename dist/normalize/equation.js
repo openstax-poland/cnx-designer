1d7b9f277b941c608aa05d6c2f3ac69b5ac95f8c
Krzysztof Mędrzycki <aiwenari@gmail.com>
2021-10-15 10:16:55 +0200

    Publish cnx-designer 0.2.4-rc.3

---
+++ dist/normalize/equation.js
@@ -0,0 +1,30 @@
+// Copyright 2021 OpenStax Poland
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license text.
+import { Element, Transforms } from 'slate';
+import { Equation } from '../interfaces';
+export default function normalizeEquation(editor, entry) {
+    const [node, path] = entry;
+    if (Equation.isEquation(node)) {
+        // Equations must not be empty
+        if (node.children.length === 0) {
+            Transforms.removeNodes(editor, { at: path });
+            return true;
+        }
+        // Equations must only have a single child
+        if (node.children.length > 1) {
+            Transforms.splitNodes(editor, { at: [...path, node.children.length - 1] });
+            return true;
+        }
+        // Equation can only contain elements from a predefined set and in
+        // a strict order.
+        if (!Element.isElement(node.children[0]) || !editor.isEquationContent(node.children[0])) {
+            Transforms.unwrapNodes(editor, {
+                at: [...path, 0],
+                match: n => n === node,
+            });
+            return true;
+        }
+    }
+    return false;
+}
