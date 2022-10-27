e8c7df9e7dd62c5f3544461c45a1c227f7e33ae0
Krzysztof Mędrzycki <aiwenari@gmail.com>
2022-10-27 09:02:04 +0200

    Publish cnx-designer 0.2.4-rc.11

---
+++ dist/util/index.js
@@ -0,0 +1,7 @@
+// Copyright 2020 OpenStax Poland
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license text.
+import * as uuid_1 from './uuid';
+export { uuid_1 as uuid };
+export { default as dedup } from './dedup';
+export { default as enumerate } from './enumerate';
