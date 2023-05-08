02db03adb6444031ba1fa7270e526793bfd70448
OneByteTwoByte <mateusz.tymiakin@openstax.pl>
2023-05-08 10:35:29 +0200

    Publish cnx-designer 0.2.4-beta1

---
+++ dist/normalize/rule.d.ts
@@ -0,0 +1,7 @@
+import { Editor, NodeEntry } from 'slate';
+/**
+ * Normalize an rule or contents of an rule
+ *
+ * Return true if entry was normalized and false otherwise.
+ */
+export default function normalizeRule(editor: Editor, entry: NodeEntry): boolean;
---
+++ dist/normalize/text.d.ts
@@ -0,0 +1,7 @@
+import { Editor, NodeEntry } from 'slate';
+/**
+ * Normalize text nodes
+ *
+ * Return true if entry was normalized and false otherwise.
+ */
+export default function normalizeText(editor: Editor, entry: NodeEntry): boolean;
