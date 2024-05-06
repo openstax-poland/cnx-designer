c1c5d5b54af2570f30a926a20183398925a3332d
OneByteTwoByte <mateusz.tymiakin@openstax.pl>
2024-05-06 14:31:53 +0200

    Publish cnx-designer 0.2.6

---
+++ dist/normalize/admonition.d.ts
@@ -0,0 +1,7 @@
+import { Editor, NodeEntry } from 'slate';
+/**
+ * Normalize an admonition
+ *
+ * Return true if entry was normalized and false otherwise.
+ */
+export default function normalizeAdmonition(editor: Editor, entry: NodeEntry): boolean;
---
+++ dist/normalize/figure.d.ts
@@ -0,0 +1,7 @@
+import { Editor, NodeEntry } from 'slate';
+/**
+ * Normalize a figure
+ *
+ * Return true if entry was normalized and false otherwise.
+ */
+export default function normalizeFigure(editor: Editor, entry: NodeEntry): boolean;
